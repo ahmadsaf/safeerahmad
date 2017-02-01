712f399076935763953c7abb44d064608b7b091d
Safeer Ahmad <ahmadsaf@sheridancollege.ca>
2017-01-31 20:05:58 -0500

    made a minor change

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * safeerahmad Theme Customizer
+ *
+ * @package safeerahmad
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function safeerahmad_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'safeerahmad_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function safeerahmad_customize_preview_js() {
+	wp_enqueue_script( 'safeerahmad_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'safeerahmad_customize_preview_js' );
---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package safeerahmad
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function safeerahmad_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'safeerahmad_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'safeerahmad_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function safeerahmad_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
